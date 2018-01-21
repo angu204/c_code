5f42d86b23e2cc6af1e0707b803186db93569f42
angu <angu204@gmail.com>
2018-01-21 09:44:43 +0800

    test

---
+++ paomadeng.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/*利用指针，实现跑马灯的效果*/
+int main(){
+ char a[] = "hello,world";
+ char *p = NULL;
+
+ p = a;
+ while(1){
+
+ while(*p != '\0'){
+   system("clear");
+   printf("%s\n",p);
+   p++;
+   sleep(1);
+  }
+ p = a;
+ }
+
+}
